aa08547f284d252e1b83b1b635be076030cbd30c
ibanpare <13278805+ibanpare@users.noreply.github.com>
2023-02-27 15:04:07 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -16,4 +16,30 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
+
+    if (playerSelection === "paper") {
+        if (computerSelection === "paper") {
+            return("It's a tie!");
+        }   else if (computerSelection === "rock") {
+              return("You win, paper beats rock!");
+        }   else if (computerSelection === "scissors") {
+              return("You lose, scissors cut paper! :(");
+        }
+    }  else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return("You lose, paper beats rock! :(");
+        }   else if (computerSelection === "rock") {
+              return("It's a tie!");
+        }   else if (computerSelection === "scissors") {
+              return("You win, rock breaks scissors!");
+        }
+    }  else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return("You win, scissors cut paper! :)");
+        }   else if (computerSelection === "rock") {
+              return("You lose, rock breaks scissors! :(");
+        }   else if (computerSelection === "scissors") {
+              return("It's a tie!");
+        }
+    } 
 }
