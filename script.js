e03c0bb2478efcf549bfb7a6752053789b1d6a8f
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-07 15:42:54 +0100

    feat: add game logic to play five rounds

--- script.js
+++ script.js
@@ -35,63 +35,55 @@ function getHumanChoice() {
     }
 }
 
-// to play a round and compare choices
-
-function playRound(computerChoice, userChoice) {
-    if (computerChoice === "rock") {
-        if (userChoice === "scissors") {
-            computerScore += 1;
-            console.log(`Computer picked ${computerChoice}. Rock beats scissors. Computer wins!`);
-        } else if (userChoice === "paper") {
-            humanScore += 1;
-            console.log(`Computer picked ${computerChoice}. Paper beats rock. You win!`);
-        } else {
-            console.log("It's a tie!");
+function playGame() {
+    // to play a round and compare choices
+    function playRound(computerChoice, userChoice) {
+        if (computerChoice === "rock") {
+            if (userChoice === "scissors") {
+                computerScore += 1;
+                alert(`Computer picked ${computerChoice}. Rock beats scissors. Computer wins!`);
+            } else if (userChoice === "paper") {
+                humanScore += 1;
+                alert(`Computer picked ${computerChoice}. Paper beats rock. You win!`);
+            } else {
+                alert("It's a tie!");
+            }
         }
-    }
 
-    if (computerChoice === "paper") {
-        if (userChoice === "rock") {
-            computerScore += 1;
-            console.log(`Computer picked ${computerChoice}. Paper beats rock. Computer wins!`);
-        } else if (userChoice === "scissors") {
-            humanScore += 1;
-            console.log(`Computer picked ${computerChoice}. Scissors beats paper. You win!`);
-        } else {
-            console.log("It's a tie!");
+        if (computerChoice === "paper") {
+            if (userChoice === "rock") {
+                computerScore += 1;
+                alert(`Computer picked ${computerChoice}. Paper beats rock. Computer wins!`);
+            } else if (userChoice === "scissors") {
+                humanScore += 1;
+                alert(`Computer picked ${computerChoice}. Scissors beats paper. You win!`);
+            } else {
+                alert("It's a tie!");
+            }
         }
-    }
 
-    if (computerChoice === "scissors") {
-        if (userChoice === "paper") {
-            computerScore += 1;
-            console.log(`Computer picked ${computerChoice}. Scissors beats paper. Computer wins!`);
-        } else if (userChoice === "rock") {
-            humanScore += 1;
-            console.log(`Computer picked ${computerChoice}. Rock beats scissors. You win!`);
-        } else {
-            console.log("It's a tie!");
+        if (computerChoice === "scissors") {
+            if (userChoice === "paper") {
+                computerScore += 1;
+                alert(`Computer picked ${computerChoice}. Scissors beats paper. Computer wins!`);
+            } else if (userChoice === "rock") {
+                humanScore += 1;
+                alert(`Computer picked ${computerChoice}. Rock beats scissors. You win!`);
+            } else {
+                alert("It's a tie!");
+            }
         }
+    }    
+    
+    alert("Welcome to a new game of rock, paper, scissors. \nThe game will last for 5 rounds.")
+
+    for (let round = 1; round < 6; round++) {
+        alert(`We are at round ${round}`);
+        playRound(getComputerChoice(),getHumanChoice());
+        alert(`The score so far is ${humanScore} - ${computerScore} for you.`);
     }
 }
-    
-playRound(getComputerChoice(),getHumanChoice());
 
-
-/*
-
-Step 6: Write the logic to play the entire game
-Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, 
-keeps track of the scores and declares a winner at the end.
-
-Create a new function named playGame.
-Move your playRound function and score variables so that they’re declared inside of the new playGame function
-Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. 
-Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. 
-You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. 
-Specifically, you may want to change the return values to something more useful.
-If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
-
-*/
+playGame();
+alert(`Game over`);
+alert(`The final score is ${humanScore} - ${computerScore} for you.`);
