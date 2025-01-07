dd22560cbf5288b659fd5150c34e29070d22e9c2
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-07 14:51:43 +0100

    feat: add logic for human choice

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function getHumanChoice() {
+    let keepGoing = true;
+
+    while(keepGoing) {
+        
+        let humanChoice = prompt("What's your choice?", "Rock, Paper or Scissors");
+        humanChoice = humanChoice.toLowerCase();
+        humanChoice = humanChoice.trim()
+
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            return humanChoice;
+        } else {
+            alert("Your choice is not valid, try again")
+        }
+    }
+}
