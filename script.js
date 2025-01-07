89512e3879b3539b37a5187b3607e8c9d6b654dc
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-07 15:16:49 +0100

    feat: add function to play a round

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// variables to track score
+
+let humanScore = 0;
+let computerScore = 0;
+
+// to randomly get computer choice
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 100);
     if (choice <= 33) {
@@ -8,6 +15,8 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+// to ask user for a choice
 
 function getHumanChoice() {
     let keepGoing = true;
@@ -26,41 +35,63 @@ function getHumanChoice() {
     }
 }
 
-/*
+// to play a round and compare choices
 
-Step 4: Declare the players score variables
-Your game will keep track of the players score. You will write variables to keep track of the players score.
+function playRound(computerChoice, userChoice) {
+    if (computerChoice === "rock") {
+        if (userChoice === "scissors") {
+            computerScore += 1;
+            console.log(`Computer picked ${computerChoice}. Rock beats scissors. Computer wins!`);
+        } else if (userChoice === "paper") {
+            humanScore += 1;
+            console.log(`Computer picked ${computerChoice}. Paper beats rock. You win!`);
+        } else {
+            console.log("It's a tie!");
+        }
+    }
 
-Create two new variables named humanScore and computerScore in the global scope.
-Initialize those variables with the value of 0.
+    if (computerChoice === "paper") {
+        if (userChoice === "rock") {
+            computerScore += 1;
+            console.log(`Computer picked ${computerChoice}. Paper beats rock. Computer wins!`);
+        } else if (userChoice === "scissors") {
+            humanScore += 1;
+            console.log(`Computer picked ${computerChoice}. Scissors beats paper. You win!`);
+        } else {
+            console.log("It's a tie!");
+        }
+    }
 
-*/
+    if (computerChoice === "scissors") {
+        if (userChoice === "paper") {
+            computerScore += 1;
+            console.log(`Computer picked ${computerChoice}. Scissors beats paper. Computer wins!`);
+        } else if (userChoice === "rock") {
+            humanScore += 1;
+            console.log(`Computer picked ${computerChoice}. Rock beats scissors. You win!`);
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+    
+playRound(getComputerChoice(),getHumanChoice());
 
-
-
-/*
-
-Step 5: Write the logic to play a single round
-Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-Create a new function named playRound.
-Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-Increment the humanScore or computerScore variable based on the round winner.
-
-*/
 
 /*
 
 Step 6: Write the logic to play the entire game
-Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
+Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, 
+keeps track of the scores and declares a winner at the end.
 
 Create a new function named playGame.
 Move your playRound function and score variables so that they’re declared inside of the new playGame function
 Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
+Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. 
+Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. 
+You need to recall the choice functions to get new choices for each round.
+Re-work your previous functions or create more helper functions if necessary. 
+Specifically, you may want to change the return values to something more useful.
 If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
 
 */
